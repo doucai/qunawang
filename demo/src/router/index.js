082d56943f4d7adf77b1769ee0403a87de6aad6f
tianda <hyu1993@sina.com>
2018-05-25 16:19:57 +0800

    异步加载组件

--- demo/src/router/index.js
+++ demo/src/router/index.js
@@ -1,8 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from '@/page/Home/Home'
-import city from '@/page/city/city'
-import detail from '@/page/detail/detail'
 Vue.use(Router)
 
 export default new Router({
@@ -11,16 +8,19 @@ export default new Router({
     {
       path: '/',
       name: 'Home',
-      component: Home
+      // 异步加载组件
+      component: ()=>import('@/page/Home/Home')
     },
     {
       path: '/city',
       name: 'city',
-      component: city
+      // 异步加载组件
+      component: ()=>import('@/page/city/city')
     },{
       path: '/detail/:id',
       name: 'detail',
-      component: detail
+      // 异步加载组件
+      component: ()=>import('@/page/detail/detail')
     }
   ],
   // 每次回到页面的时候保持初始值为X:0,y:0
